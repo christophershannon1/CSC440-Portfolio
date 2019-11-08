c82cc327fd2480ba258ec3d199792e86fe33ab80
christophershannon1 <54648551+christophershannon1@users.noreply.github.com>
2019-11-07 19:12:29 -0800

    Add files via upload

---
+++ CSC440 002/CSC440 002/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSC440_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variables need to be declared before they can be used
+            //the 'int' before the variables indicate that i wish to assign integer values into the variables
+            //the value of the integer in C# can be between -2,147,000,000 and 2,147,000,000
+            int x;
+            int y;
+
+            //the '=' is called an assignment operator.
+            //the assignment operator takes what is on the right hand side and assigns that into whatever is on the left hand side.
+            x = 7;
+            y = x + 3;
+
+            //the following line of code outputs the value of the y variable
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CSC440 003/CSC440 003/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSC440_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //the following code will ask the user to enter their first name and last name and output both
+            //the '+' will concatinate the two strings together into one larger string
+            Console.WriteLine("What is your name?");
+            //unlike with 'WriteLine' 'Write' will not create a new line
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            //by using 'ReadLine' method in this instance the user can input data
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name: ");
+            myLastName = Console.ReadLine();
+
+            //the two strings being added together will have a space in between them for easier reading
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+        }
+    }
+}
