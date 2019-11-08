e4eeac5dc8aee211a2e67214261576717c6aadfe
christophershannon1 <54648551+christophershannon1@users.noreply.github.com>
2019-11-07 18:18:31 -0800

    Add files via upload

---
+++ CSC440 001/CSC440 001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//the namespace has the name of the application given by the coder
+namespace CSC440_001
+{
+    //the following class is called 'Program' and is a container for all of the methods of the application
+    class Program
+    {
+        //the following code block is called a 'method' and is called 'Main'
+        static void Main(string[] args)
+        {
+            //the following line of code is used to stick text in a window
+            //the '.' is called the 'member accesser'
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, LAC25!");
+            //tells application to wait for input from user before continuing execution.
+            Console.ReadLine();
+        }
+    }
+}
